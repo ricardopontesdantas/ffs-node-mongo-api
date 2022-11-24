9d8f7b95cd8aa2db9f77fa1c4cbae4fea9d99ef8
ricardopontesdantas <ricardopontesdantas@gmail.com>
2022-11-24 05:32:37 -0300

    add get method

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const ProductsController = require('../controllers/products')
 
-router.get('/api/products', ProductsController.get)
+router.get('/api/products/:id?', ProductsController.get)
 // router.post('/api/products', ProductsController.post)
 // router.put('/api/products/:id', ProductsController.put)
 // router.delete('/api/products/:id', ProductsController.delete)
